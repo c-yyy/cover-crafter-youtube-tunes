ccb966a81ab49a26737a0f21a8e31d6f76c33279
cy-mac-mini4 <mac-mini4@cy.com>
2025-06-25 00:23:53 +0800

    chore: 移除不再使用的lovable-tagger依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import sitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
@@ -14,7 +13,6 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     mode === 'development' &&
-    componentTagger(),
     sitemap({
       hostname: 'https://youtube-cover.com',
       dynamicRoutes: [
